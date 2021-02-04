6ca1a01f9da98a1ec7a3aff42e88bfcf6b997fb5
Bunyodbek Sherazimov <bsherazimov@gmail.com>
2021-02-04 15:31:55 +0500

    added filter product

--- src/components/filteredProducts/filteredProducts.jsx
+++ src/components/filteredProducts/filteredProducts.jsx
@@ -1,23 +1,218 @@
 import React, { Component } from 'react';
-import st from './filteredProducts.module.css'
+import st from './filteredProducts.module.css';
+import { FaRegHeart, FaStar, FaCard } from "react-icons/fa";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
 
 class FilteredProducts extends Component {
     render() { 
         return (  
             <>
-                {/* <div className={`${st.secondary_filter}`}>
-                    <h2 className={`${st.filter_title}`}>Product Name</h2>
-                        <div className={`${st.secondary_filter__box}`}>
-                        
-                     </div>
-                </div>
                 <div className={`${st.filtered_products__wrapper}`}>
-                    <div className={`${st.filtered_product__box}`}>
-                        <div className={`${st.filtered_product__box__img}`}>
-                            
+                            <div className={`${st.filtered_product__box}`}>
+                                <div className={`${st.filtered_product__box__img}`}>
+                                {/* rasm uchun joy */}
+                                </div>
+                                <div className={`${st.filtered_product__box__content}`}>
+                                    <h4>Lorem, ipsum.</h4>
+                                    <div className={`${st.star_list}`}>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                    </div>
+                                    <div className={`${st.price_and_shop}`}>
+                                        <span className={`${st.product_price}`}>$ 49.99</span>
+                                        <div className={`${st.product_add__card}`}>
+                                            <AiOutlineShoppingCart className={`${st.shopcart_icon}`}/>
+                                            <span>Savatga qo'shish</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={`${st.heart_icon__wrapper}`}>
+                                    <FaRegHeart className={`${st.heart_icon}`}/>
+                                </div>
+                            </div> 
+                            <div className={`${st.filtered_product__box}`}>
+                                <div className={`${st.filtered_product__box__img}`}>
+                                {/* rasm uchun joy */}
+                                </div>
+                                <div className={`${st.filtered_product__box__content}`}>
+                                    <h4>Lorem, ipsum.</h4>
+                                    <div className={`${st.star_list}`}>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                    </div>
+                                    <div className={`${st.price_and_shop}`}>
+                                        <span className={`${st.product_price}`}>$ 49.99</span>
+                                        <div className={`${st.product_add__card}`}>
+                                            <AiOutlineShoppingCart className={`${st.shopcart_icon}`}/>
+                                            <span>Savatga qo'shish</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={`${st.heart_icon__wrapper}`}>
+                                    <FaRegHeart className={`${st.heart_icon}`}/>
+                                </div>
+                            </div> 
+                            <div className={`${st.filtered_product__box}`}>
+                                <div className={`${st.filtered_product__box__img}`}>
+                                {/* rasm uchun joy */}
+                                </div>
+                                <div className={`${st.filtered_product__box__content}`}>
+                                    <h4>Lorem, ipsum.</h4>
+                                    <div className={`${st.star_list}`}>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                    </div>
+                                    <div className={`${st.price_and_shop}`}>
+                                        <span className={`${st.product_price}`}>$ 49.99</span>
+                                        <div className={`${st.product_add__card}`}>
+                                            <AiOutlineShoppingCart className={`${st.shopcart_icon}`}/>
+                                            <span>Savatga qo'shish</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={`${st.heart_icon__wrapper}`}>
+                                    <FaRegHeart className={`${st.heart_icon}`}/>
+                                </div>
+                            </div> 
+                            <div className={`${st.filtered_product__box}`}>
+                                <div className={`${st.filtered_product__box__img}`}>
+                                {/* rasm uchun joy */}
+                                </div>
+                                <div className={`${st.filtered_product__box__content}`}>
+                                    <h4>Lorem, ipsum.</h4>
+                                    <div className={`${st.star_list}`}>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                    </div>
+                                    <div className={`${st.price_and_shop}`}>
+                                        <span className={`${st.product_price}`}>$ 49.99</span>
+                                        <div className={`${st.product_add__card}`}>
+                                            <AiOutlineShoppingCart className={`${st.shopcart_icon}`}/>
+                                            <span>Savatga qo'shish</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={`${st.heart_icon__wrapper}`}>
+                                    <FaRegHeart className={`${st.heart_icon}`}/>
+                                </div>
+                            </div>   
+                            <div className={`${st.filtered_product__box}`}>
+                                <div className={`${st.filtered_product__box__img}`}>
+                                {/* rasm uchun joy */}
+                                </div>
+                                <div className={`${st.filtered_product__box__content}`}>
+                                    <h4>Lorem, ipsum.</h4>
+                                    <div className={`${st.star_list}`}>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                    </div>
+                                    <div className={`${st.price_and_shop}`}>
+                                        <span className={`${st.product_price}`}>$ 49.99</span>
+                                        <div className={`${st.product_add__card}`}>
+                                            <AiOutlineShoppingCart className={`${st.shopcart_icon}`}/>
+                                            <span>Savatga qo'shish</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={`${st.heart_icon__wrapper}`}>
+                                    <FaRegHeart className={`${st.heart_icon}`}/>
+                                </div>
+                            </div>        
+                            <div className={`${st.filtered_product__box}`}>
+                                <div className={`${st.filtered_product__box__img}`}>
+                                {/* rasm uchun joy */}
+                                </div>
+                                <div className={`${st.filtered_product__box__content}`}>
+                                    <h4>Lorem, ipsum.</h4>
+                                    <div className={`${st.star_list}`}>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                    </div>
+                                    <div className={`${st.price_and_shop}`}>
+                                        <span className={`${st.product_price}`}>$ 49.99</span>
+                                        <div className={`${st.product_add__card}`}>
+                                            <AiOutlineShoppingCart className={`${st.shopcart_icon}`}/>
+                                            <span>Savatga qo'shish</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={`${st.heart_icon__wrapper}`}>
+                                    <FaRegHeart className={`${st.heart_icon}`}/>
+                                </div>
+                            </div>        
+                            <div className={`${st.filtered_product__box}`}>
+                                <div className={`${st.filtered_product__box__img}`}>
+                                {/* rasm uchun joy */}
+                                </div>
+                                <div className={`${st.filtered_product__box__content}`}>
+                                    <h4>Lorem, ipsum.</h4>
+                                    <div className={`${st.star_list}`}>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                    </div>
+                                    <div className={`${st.price_and_shop}`}>
+                                        <span className={`${st.product_price}`}>$ 49.99</span>
+                                        <div className={`${st.product_add__card}`}>
+                                            <AiOutlineShoppingCart className={`${st.shopcart_icon}`}/>
+                                            <span>Savatga qo'shish</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={`${st.heart_icon__wrapper}`}>
+                                    <FaRegHeart className={`${st.heart_icon}`}/>
+                                </div>
+                            </div>        
+
+          <div className={`${st.filtered_product__box}`}>
+                                <div className={`${st.filtered_product__box__img}`}>
+                                {/* rasm uchun joy */}
+                                </div>
+                                <div className={`${st.filtered_product__box__content}`}>
+                                    <h4>Lorem, ipsum.</h4>
+                                    <div className={`${st.star_list}`}>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                            <FaStar className={`${st.star_icon}`}/>
+                                    </div>
+                                    <div className={`${st.price_and_shop}`}>
+                                        <span className={`${st.product_price}`}>$ 49.99</span>
+                                        <div className={`${st.product_add__card}`}>
+                                            <AiOutlineShoppingCart className={`${st.shopcart_icon}`}/>
+                                            <span>Savatga qo'shish</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={`${st.heart_icon__wrapper}`}>
+                                    <FaRegHeart className={`${st.heart_icon}`}/>
+                                </div>
+                            </div>        
+          
+               
                         </div>
-                    </div>
-                </div> */}
             </>
         );
     }
