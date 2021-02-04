8882e95933cf207526802f29134c9859a5e2b5e6
Bunyodbek Sherazimov <bsherazimov@gmail.com>
2021-02-04 11:18:33 +0500

    added app js file

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ class App extends Component {
         return (
             <div>
                 <Layout>
-                    <Home/>
-                    {/* <Filter/> */}
+                    {/* <Home/> */}
+                    <Filter/>
                 </Layout>
             </div>
         )
